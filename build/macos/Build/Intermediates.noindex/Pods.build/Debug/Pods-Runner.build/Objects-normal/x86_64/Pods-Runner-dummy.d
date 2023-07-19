29f75c722c640b450df0ed83c2b1949423c8c8d3
VikuDev69 <vikkubharti69@gmail.com>
2023-07-19 16:09:27 +0530

    added all files

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/viku/Project/task_1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/viku/Project/task_1/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
